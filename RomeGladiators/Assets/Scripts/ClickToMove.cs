1de503c30b522c3efff4da2d99cf258c90775b7f
Denis <dsivtsov@yahoo.com>
2022-09-30 21:09:39 +0500

    init

---
+++ RomeGladiators/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+    RaycastHit hitInfo = new RaycastHit();
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+    }
+
+    Vector3 time;
+    Ray ray;
+    void Update () {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+            ray = Camera.main.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 destination = hitInfo.point;
+                //destination = transform.position + Vector3.forward * 20;
+                agent.destination = destination;
+                GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Cube.transform.position = destination;
+                Debug.Log($"agent.nextPosition={agent.nextPosition} agent.nextPosition={agent.destination}");
+            }
+
+
+        }
+    }
+}
---
+++ RomeGladiators/Assets/Scripts/LocomotionSimpleAgent.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+[RequireComponent (typeof (Animator))]
+public class LocomotionSimpleAgent : MonoBehaviour {
+    Animator anim;
+    UnityEngine.AI.NavMeshAgent agent;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+    LookAt lookAt;
+    void Start () {
+        anim = GetComponent<Animator> ();
+        //lookAt = GetComponent<LookAt>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+        agent.updatePosition = false;
+    }
+    
+    void Update ()
+    {
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+        
+
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot (transform.right, worldDeltaPosition);
+        float dz = Vector3.Dot (transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2 (dx, dz);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
+        smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if delta time is safe
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+
+        // Update animation parameters
+        anim.SetBool("Move", shouldMove);
+        anim.SetFloat("XVelocity", velocity.x);
+        anim.SetFloat("ZVelocity", velocity.y);
+        if (shouldMove)
+        {
+            Debug.Log($"agent.nextPosition={agent.nextPosition} worldDeltaPosition={worldDeltaPosition} velocity={velocity.magnitude}"); 
+        }
+
+        //		// Pull character towards agent
+        //		if (worldDeltaPosition.magnitude > agent.radius)
+        //			transform.position = agent.nextPosition - 0.9f*worldDeltaPosition;
+
+        //		// Pull agent towards character
+        //		if (worldDeltaPosition.magnitude > agent.radius)
+        //			agent.nextPosition = transform.position + 0.9f*worldDeltaPosition;
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update postion to agent position
+        //		transform.position = agent.nextPosition;
+
+        // Update position based on animation movement using navigation surface height
+        Vector3 position = anim.rootPosition;
+        position.y = agent.nextPosition.y;
+        transform.position = position;
+    }
+}
